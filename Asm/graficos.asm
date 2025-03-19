131337fae5cd795ca1a5d8b28e54fa37f59f6f52
Leonardo Montenegro <leomontenegro6@gmail.com>
2025-03-18 22:32:15 -0300

    Corrigido bugs causados na VWF da fonte grande, devido a ajustes na VWF da fonte pequena.

--- Asm/graficos.asm
+++ Asm/graficos.asm
@@ -4,11 +4,10 @@
 .open "chod.gba", 0x08000000
 
 ; Inserindo fontes grande e pequena.
-.org 0x08495FAB
-    .incbin "Graficos/Editados/0x495FAB - Fonte pequena.gba"
-
 .org 0x08496314
     .incbin "Graficos/Editados/0x496314 - Fonte grande.gba"
+.org 0x08495FAB
+    .incbin "Graficos/Editados/0x495FAB - Fonte pequena.gba"
 
 ; Catalogando ponteiros de gráficos comprimidos e outros dados,
 ; para que sejam posteriormente atualizados, ao reinserir
@@ -19,8 +18,10 @@
     .dw creditos
 .org 0x08164C08
     .dw creditos_tilemap
+.org 0x08014D14
+    .dw tabela_vwf_fonte_grande
 .org 0x08014f54
-    .dw tabela_vwf_pequena_letras_adicionais
+    .dw tabela_vwf_fonte_pequena
 
 ; Inserindo gráficos comprimidos editados no final da rom.
 .org 0x08FB5918
@@ -36,10 +37,13 @@ creditos_tilemap:
     .incbin "Graficos/Comprimidos/0xFA8DC0 - Creditos (Tilemap).gba"
     .align
 
-; Ajustes menores na VWF da fonte pequena, de modo a corrigir larguras
+; Ajustes menores na tabela das fontes VWF, de modo a corrigir larguras
 ; de caracteres acentuados específicos.
-tabela_vwf_pequena_letras_adicionais:
-    .incbin "Asm/tabela_vwf_pequena_letras_adicionais.bin"
+tabela_vwf_fonte_grande:
+    .incbin "Asm/tabela_vwf_fonte_grande.bin"
+    .align
+tabela_vwf_fonte_pequena:
+    .incbin "Asm/tabela_vwf_fonte_pequena.bin"
     .align
 
 ; Salvando a rom modificada.
